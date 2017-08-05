6c17542990924bcddaa29f52099d6ef8470163aa
Nicole Gasperini <nicole.gasperini88@gmail.com>
2017-08-04 21:53:09 -0500

    Added input for each letter. Stuck on do loop not working

--- game.rb
+++ game.rb
@@ -6,15 +6,37 @@
 #current guesses for them to try again
 
 class Game
+attr_reader :placeholder, :word, :letter
+
 
 	def initialize(word)
-		@word = word
+		@word = word.split('')
+		@placeholder = ""
+		@letter = ''
 	end
 
 	def create_placeholder
+		@placeholder = ""
+		@word.length.times do 
+			@placeholder += "-"
+		end
+		@placeholder.split('')
+		p placeholder
 	end
 
-	def check_guesses(guesses)
+	def check_guesses(letter)
+		@letter = letter
+		p letter
+		@word.split
+		@word.each do |char|
+			p char
+			if letter == char
+				char_idx = word.index(char)
+				placeholder[char_index].replace(char)
+			end
+			p placeholder
+		end
+		
 	end
 
 end
@@ -22,9 +44,11 @@ end
 p "Welcome to the word game! Player 1, please give a word"
 word = gets.chomp
 game = Game.new(word)
+game.create_placeholder
 
 p "Player 2, please guess a letter"
 letter = gets.chomp
+game.check_guesses(letter)
 
 
 
